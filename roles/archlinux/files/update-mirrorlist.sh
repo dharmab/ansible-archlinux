a705ea9d111bd3f594225157dd8a1d0b71bc60e1
Dharma Bellamkonda <dharma.bellamkonda@gmail.com>
2019-11-21 17:33:08 -0700

    Fix mirrorlist error handling

--- roles/archlinux/files/update-mirrorlist.sh
+++ roles/archlinux/files/update-mirrorlist.sh
@@ -3,11 +3,14 @@ mirrorlist=/etc/pacman.d/mirrorlist
 
 if find $mirrorlist -mtime +1 | grep -q '.'; then
   echo "Updating $mirrorlist..."
-  reflector \
-	--protocol https \
+  if ! reflector \
+    --protocol https \
     --country "United States" \
     --latest 32 \
-	--sort rate \
-	--save $mirrorlist
+    --sort rate \
+    --save $mirrorlist; then
+    echo "Unable to update $mirrorlist"
+    exit 1
+  fi
 fi
 echo "$mirrorlist is up to date!"
