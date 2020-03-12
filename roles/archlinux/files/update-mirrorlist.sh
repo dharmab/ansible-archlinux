932cd245b0ff57be43b4d91d4c0426ccec2d4c61
Dharma Bellamkonda <dharma.bellamkonda@gmail.com>
2020-03-12 00:18:00 -0600

    Consistent shell indentation

--- roles/archlinux/files/update-mirrorlist.sh
+++ roles/archlinux/files/update-mirrorlist.sh
@@ -2,15 +2,15 @@
 mirrorlist=/etc/pacman.d/mirrorlist
 
 if find $mirrorlist -mtime +1 | grep -q '.'; then
-  echo "Updating $mirrorlist..."
-  if ! reflector \
-    --protocol https \
-    --country "United States" \
-    --latest 32 \
-    --sort rate \
-    --save $mirrorlist; then
-    echo "Unable to update $mirrorlist"
-    exit 1
-  fi
+    echo "Updating $mirrorlist..."
+    if ! reflector \
+        --protocol https \
+        --country "United States" \
+        --latest 32 \
+        --sort rate \
+        --save $mirrorlist; then
+        echo "Unable to update $mirrorlist"
+        exit 1
+    fi
 fi
 echo "$mirrorlist is up to date!"
