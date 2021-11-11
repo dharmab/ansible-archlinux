895ed238a21b0551a2def057e2684ffefb77d517
Dharma Bellamkonda <dharma.bellamkonda@gmail.com>
2021-11-11 08:56:51 -0700

    More linting

--- lint.sh
+++ lint.sh
@@ -3,6 +3,8 @@
 project_root="$(git rev-parse --show-toplevel)"
 
 exit_code=0
-poetry run ansible-lint "$project_root/site.yml" || exit_code=$?
+
+poetry run yamllint "$project_root" || exit_code=$?
+poetry run ansible-lint -qq "$project_root/site.yml" || exit_code=$?
 
 exit $exit_code
