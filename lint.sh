94b8189826adec2a956b7aaa1a14fceb3bc43d85
Dharma Bellamkonda <dharma.bellamkonda@gmail.com>
2025-02-17 02:15:32 -0700

    moar fixes

--- lint.sh
+++ lint.sh
@@ -4,7 +4,6 @@ project_root="$(git rev-parse --show-toplevel)"
 
 exit_code=0
 
-poetry run yamllint "$project_root" || exit_code=$?
-poetry run ansible-lint -qq "$project_root/site.yml" || exit_code=$?
+uv run ansible-lint -qq "$project_root/site.yml" || exit_code=$?
 
 exit $exit_code
